b79ebc9196b2b39fa4eff08ff22c6672a3a0b0cc
swttpch <igor.souza@ecto.digital>
2025-02-21 19:37:17 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const version = await database
+    .query("SHOW server_version;")
+    .then((res) => res.rows[0].server_version);
+  const maxConnections = await database
+    .query("SHOW max_connections;")
+    .then((res) => res.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database
+    .query({
+      text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    })
+    .then((res) => res.rows[0]);
   response.status(200).json({
-    message: "everything is fine",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        opened_connections: activeConnections.count,
+      },
+    },
   });
 }
 export default status;
