88c880e70da7bdbad01a54d25b47136c30985465
swttpch <igor.souza@ecto.digital>
2025-03-11 11:05:03 -0300

    test: rename all descriptions to use new pattern
    git push

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,25 +3,38 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await cleanDatabase();
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 });
 
-async function cleanDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response.status).toBe(201);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Runing pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
 
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-  const nextResponse = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(200);
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  const nextResponseBody = await nextResponse.json();
-  expect(Array.isArray(nextResponseBody)).toBe(true);
-  expect(nextResponseBody.length).toBe(0);
 });
