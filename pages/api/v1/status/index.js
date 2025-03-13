9bc01df4e42b940ebfa507e7d3740f8e5d5e514a
swttpch <igor.souza@ecto.digital>
2025-03-13 14:51:07 -0300

    refactor: abstracting `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,42 +1,56 @@
-import { InternalServerError } from "infra/errors";
+import { createRouter } from "next-connect";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
 import database from "infra/database.js";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
-    const version = await database
-      .query("SHOW server_version;")
-      .then((res) => res.rows[0].server_version);
-    const maxConnections = await database
-      .query("SHOW max_connections;")
-      .then((res) => res.rows[0].max_connections);
+const router = createRouter();
 
-    const databaseName = process.env.POSTGRES_DB;
-    const activeConnections = await database
-      .query({
-        text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
-        values: [databaseName],
-      })
-      .then((res) => res.rows[0]);
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: version,
-          max_connections: parseInt(maxConnections),
-          opened_connections: activeConnections.count,
-        },
-      },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
+router.get(getHandler);
 
-    console.log("\n Error dentro do catch do controller: ");
-    console.error(publicErrorObject);
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
 
-    response.status(500).json(publicErrorObject);
-  }
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
-export default status;
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.log("\n Error dentro do onErrorHandler do next-connect:");
+  console.error(publicErrorObject);
+
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+  const version = await database
+    .query("SHOW server_version;")
+    .then((res) => res.rows[0].server_version);
+  const maxConnections = await database
+    .query("SHOW max_connections;")
+    .then((res) => res.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database
+    .query({
+      text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    })
+    .then((res) => res.rows[0]);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        opened_connections: activeConnections.count,
+      },
+    },
+  });
+}
---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,23 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+      expect(response.status).toBe(405);
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Method Not Allowed",
+        action: "Verify if the HTTP method is correct",
+        status_code: 405,
+      });
+    });
+  });
+});
